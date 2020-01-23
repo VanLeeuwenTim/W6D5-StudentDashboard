fa7742e8e6cb59306aa0837151b194d889158aa6
Tim van Leeuwen <timvanleeuwen06@gmail.com>
2020-01-23 09:11:19 +0100

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from "react";
+import Container from "./Container";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Test</h1>
-      </header>
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/">
+          <Container />
+        </Route>
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>Hallo en welkom op mijn mooie studentdashboard</p>
+  </div>
+);
 
 export default App;
---
+++ src/Container.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Container extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Container test</h1>
+      </div>
+    );
+  }
+}
+
+export default Container;
